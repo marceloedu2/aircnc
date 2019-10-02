d0bb5a5a8fdf5dd9acf0226b4d07c404d1834200
marceloedu2 <marcelo.edu2@gmail.com>
2019-10-02 19:12:33 -0300

    configurando gravação de empresas

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,28 +1,75 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import api from  '../../services/api'
 
-// import { Container } from './styles';
+import camera from '../../assets/img/camera.svg'
 
-export default function New() {
+import './styles.css'
+
+export default function New({ history }) {
   const [company, setCompany] = useState('')
+  const [techs, setTechs] = useState('')
+  const [price, setPrice] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
 
-  function handleSubmit() {
+  const preview = useMemo(
+    () => {
+      return thumbnail ? URL.createObjectURL(thumbnail) : null
+    },[thumbnail]
+  )
 
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const data = new FormData()
+    const user_id = localStorage.getItem('user')
+
+    data.append('thumbnail', thumbnail)
+    data.append('company', company)
+    data.append('techs', techs)
+    data.append('price', price)
+
+    await api.post('/spots', data, {
+      headers: { user_id }
+    })
+
+    history.push('/dashboard')
   }
 
   return (
-    <form onSubmit={}>
+    <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{backgroundImage: `url(${preview})`}}
+        className={thumbnail ? 'has-thumbnail' : ''}
+      >
+        <input
+          type="file"
+          onChange={event => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} alt="select img" />
+      </label>
       <label htmlFor="company">EMPRESA *</label>
       <input
         id="company"
-        placeholder="Sua empresa incrÌvel"
-        value={event => setCompany(event.target.value)}
+        placeholder="Sua empresa incr√≠vel"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
       />
-      <label htmlFor="company">TECNOLOGIAS * <span>(separadas por vÌrgulas)</span></label>
+      <label htmlFor="company">TECNOLOGIAS * <span>(separadas por v√≠rgulas)</span></label>
       <input
         id="company"
-        placeholder="Sua empresa incrÌvel"
-        value={event => setCompany(event.target.value)}
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
       />
+      <label htmlFor="price">VALOR DA DI√ÅRIA * <span>(em branco para GRATUITO)</span></label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+      <button type="submit" className="btn">Cadastrar</button>
     </form>
   );
 }
